407207236a1b3434ec025c0e491d5b25b91c041b
Pierre Lafievre <pierre.lafievre@iguanesolutions.com>
2023-12-20 16:30:37 +0100

    test

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/hekmon/pushover/v2
+module github.com/treywelsh/pushover/v2
 
 go 1.14
 
--- send.go
+++ send.go
@@ -2,6 +2,7 @@ package pushover
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/gregdel/pushover"
@@ -83,10 +84,12 @@ func (c *Controller) SendCustomMsg(msg Message) error {
 	}
 	// send it
 	response, err := c.app.SendMessage(&raw, c.dest)
+	log.Printf("response:%s\n====\nerr:%s", response, err)
 	if err != nil {
+		log.Printf("err not nil\n")
 		return fmt.Errorf("sending fail: %w", err)
 	} else if response != nil && response.Status != 1 {
-
+		log.Printf("response not nil and status !=1\n")
 		isLimitReached := response.Limit != nil && response.Limit.Remaining == 0
 		err := ServerError{
 			Errors:       response.Errors,
